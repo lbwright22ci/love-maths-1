75284e3f948df4fedb160277dfba33e649a200da
lbwright22ci <lbwright22@gmail.com>
2026-01-03 10:09:10 +0000

    add js structure

---
+++ assets/js/script.js
@@ -0,0 +1,31 @@
+function runGame(){
+
+}
+
+function checkAnswer(){
+
+}
+
+function calculateCorrectAnswer(){
+
+}
+
+function incrementScore(){
+
+}
+
+function incrementIncorrectScore(){
+
+}
+
+function displayAdditionQuestion(){
+
+}
+
+function displaySubtractQuestion(){
+
+}
+
+function displayMultiplyQuestion(){
+    
+}
