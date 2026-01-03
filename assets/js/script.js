6e28245ca1c2aaefb3941ecec335f81ff6ff53fc
lbwright22ci <lbwright22@gmail.com>
2026-01-03 10:35:23 +0000

    add code for generating two random numbers

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,21 @@
+//Wait for DOM to finish loading
+
+document.addEventListener("DOMContentLoaded", function(){
+    let buttons = document.getElementsByTagName("button");
+    for(let button of buttons){
+        button.addEventListener('click', function(){
+            if(this.getAttribute("data-type") === "submit"){
+                alert("You clicked submit");
+            } else{
+                let gameType = this.getAttribute("data-type");
+                alert(`You clicked ${gameType}`);
+            }
+        })
+    }
+})
 function runGame(){
+    let num1 = Math.floor(Math.random()*25)+1;
+    let num2 = Math.floor(Math.random()*25)+1;
 
 }
 
@@ -27,5 +44,5 @@ function displaySubtractQuestion(){
 }
 
 function displayMultiplyQuestion(){
-    
+
 }
