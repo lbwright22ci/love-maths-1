dd8730f4cde9f4260451b674db87a59e4023c4f1
lbwright22ci <lbwright22@gmail.com>
2026-01-04 12:46:16 +0000

    add code tidy up

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,6 +12,12 @@ document.addEventListener("DOMContentLoaded", function(){
             }
         })
     }
+
+    document.getElementById("answer-box").addEventListener("keydown", function(event){
+        if(event.key === "Enter"){
+            checkAnswer();
+        }
+    })
     runGame("addition");
 })
 
@@ -22,6 +28,9 @@ function runGame(gameType){
     let num1 = Math.floor(Math.random()*25)+1;
     let num2 = Math.floor(Math.random()*25)+1;
 
+    document.getElementById("answer-box").value = "";
+    document.getElementById("answer-box").focus();
+    
     if(gameType ==="addition"){
         displayAdditionQuestion(num1, num2);
     }else if(gameType ==="subtract"){
